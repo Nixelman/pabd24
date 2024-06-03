f3219d5234b312506b5631249afc205d0576cd6d
Nixelman <oleggelof@gmail.com>
2024-06-03 18:59:19 +0300

    Change parameters

--- src/predict_app.py
+++ src/predict_app.py
@@ -52,7 +52,7 @@ def predict_web_serve():
     in_data = request.get_json()
     # price = predict(in_data)
     area = in_data["area"]
-    price = predict_cpu_multithread(area)
+    price = predict_io_bounded(area)
     return {'price': price}
 
 
