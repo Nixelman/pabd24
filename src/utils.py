0fc5be0a68c618cad8593d440ceeb2ab888b211c
Nixelman <oleggelof@gmail.com>
2024-06-03 18:14:56 +0300

    Change parameters utils.py

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ import numpy as np
 def predict_io_bounded(area):
     """Emulate io delay"""
     time.sleep(1)
-    avg_price = 200_000                 # RUB / m2
+    avg_price = 50_000                 # RUB / m2
     return int(area * avg_price)
 
 
