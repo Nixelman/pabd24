0ff5bd5a1b3a63b8ca57c1b5ada110f35f02845d
Nixelman <oleggelof@gmail.com>
2024-06-15 12:29:42 +0300

    Changed predict_app.py

--- src/predict_app.py
+++ src/predict_app.py
@@ -4,8 +4,6 @@ from flask import Flask, request
 from flask_cors import CORS
 from joblib import load
 from flask_httpauth import HTTPTokenAuth
-# from src.utils import *
-# from utils import *
 
 
 MODEL_SAVE_PATH = 'models/linear_regression_v01.joblib'
